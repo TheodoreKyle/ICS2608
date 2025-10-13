7268890feb1b6f3b66289c08abaf16c2459b22ae
|   Ascension   | <theodorekyle.choa.0315@gmail.com>
2025-10-13 15:56:28 +0800

    commit

---
+++ October 13, 2025/script.js
@@ -0,0 +1,10 @@
+// confirm("Are you sure?"); 
+let greeting = "whatdsdtsdt!!!"; 
+alert(greeting); 
+
+let heading = document.getElementById("heading");
+
+function changeText() {
+    heading.textContent = greeting; 
+    heading.style.color = "blue";  
+}
