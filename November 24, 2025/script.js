181efd0d80bc646da621045d282a32fa00e2b91f
|   Ascension   | <theodorekyle.choa.0315@gmail.com>
2025-11-24 16:28:35 +0800

    JavaScript Objects

--- November 24, 2025/script.js
+++ November 24, 2025/script.js
@@ -1,9 +1,19 @@
-let nums = [1,2,3,4,5];
-console.log(nums.length);
+let nums = [1,2,3];
+let copynum = [...nums];
+// let copynum = nums; 
+// nums.push(4); 
+// nums = [3,4,5];
+
+copynum.push(4);
+
+console.log(nums);
+console.log(copynum);
+
 
 let person = {
     name: "Theo",
     age: 19,
+    gender: "Male",
     favSubject: ['Math', 'English', 'Filipino'],
     pet: {
         petName: 'Junjun',
@@ -11,20 +21,80 @@ let person = {
     },
     greet: function() {
         console.log(`Hello User: ${this.name}`);
-    }
+    },
+    yr: "2nd",
 } 
-person.name = "Gabriel";
-person.age = 32; 
-person.gender = "male"; 
 
-// console.log(`Pet: ${person.pet.petName} 
-// Type: ${person.pet.type}`);
+// let {name: pangalan, age: edad, gender, favSubject, yr = "Not Enrolled"} = person; 
+// console.log(`My name is ${pangalan}
+// I am ${edad}yrs old`);
+// console.log(`Favorite Subject: ${favSubject[0]}`);
+// console.log(yr);
 
-person.greet(); 
 
-// let person = new Object(); 
-// person.name = "Theo";
-// person.age = 19;
 
-// console.log(person);
-// console.log(`Hello ${person["name"]} ${person.age}`);
+// // console.log(person.favSubject[0]);
+
+// // person.name = "Gabriel";
+// // person.age = 32;
+
+// // person.greet(); 
+
+// // console.log(`Pet: ${person.pet.petName} 
+// // Type: ${person.pet.type}`);
+
+
+
+// // // let person2 = new Object(); 
+// // person2.name = "Theo";
+// // person2.age = 19;
+
+// // console.log(person2);
+// // console.log(`Hello ${person2["name"]} ${person2.age}`);
+
+// let user = {
+//     name: "Theodore Kyle Choa",
+//     age: 19,
+//     gender: "Male",
+//     username: "|   Ascension   |",
+//     password: "123456",
+// }
+
+// let { name, age, ...propetc } = user; 
+
+// let course = {
+//     courseCode: 'ICS2608',
+//     courseName: 'App Dev 1',
+//     courseDescription: 'Web Frontend Development Using HTML, CSS, and Javascript',
+// }
+
+// let student01 = { name, age, ...course }; 
+// console.log(student01);
+
+// console.log(user);
+
+// let {age, name, gender, ...logindetails} = user;
+
+// console.log(name);
+// console.log(age);
+// console.log(logindetails);
+
+// let {username, password} = user;
+// console.log(`Username: ${username}
+// Password: ${password}`);
+
+let settings = {
+    theme: 'dark',
+    notifications: true,
+}
+// settings.notifications = false;
+let updatedSettings = {...settings, notifications: false}
+
+console.log(settings);
+console.log(updatedSettings);
+
+function addNum(num, ...numetc) {
+    console.log(num);
+    console.log(...numetc);
+}
+addNum(1,2,3,4,5,6,7,8);
